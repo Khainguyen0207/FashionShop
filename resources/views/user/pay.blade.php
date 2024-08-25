751c06dc0b9a729f94c52bcc26964a9bb9125bc6
KhaiNguyen0207 <tkhai12386@gmail.com>
2024-08-25 22:56:24 +0700

    update

--- resources/views/user/pay.blade.php
+++ resources/views/user/pay.blade.php
@@ -2,6 +2,7 @@
 @push('head')
     <link rel="shortcut icon" href="{{asset('assets/user/img/logo.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{asset('assets/user/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/user/css/pay.css')}}">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     
@@ -10,7 +11,7 @@
 @section('content')
     @include('layouts.user.header')
     <div id="container">
-
+        
     </div>
     @include('layouts.user.footer')
 @endsection
