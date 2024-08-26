43f0722c36b9351ffd28fadb4cefaecbef237232
KhaiNguyen0207 <tkhai12386@gmail.com>
2024-08-27 00:14:18 +0700

    update

--- resources/views/user/pay.blade.php
+++ resources/views/user/pay.blade.php
@@ -18,7 +18,7 @@
             <div class="content">
                 <div class="recipient-information">
                     <h3 class="item">Thông tin người nhận</h3>
-                    <form action="?" method="get">
+                    <form action="?" method="get" class="information">
                         <label for="recipient-name">Tên người nhận</label>
                         <input type="text" name="recipient-name" id="recipient-name" spellcheck="false" max="255" maxlength="100">
                         <label for="number-phone" >Số điện thoại</label>
@@ -45,17 +45,25 @@
                 <div class="payment-method">
                     <h3 class="item">Phương thức thanh toán</h3>
                     <h5>Chọn phương thức thanh toán</h5>
-                    <form action="" method="GET"class="select-banking">
-                        <a href="?momo" class="banking"><img src="{{asset('assets/img/momo.png')}}" alt=""></a>
-                        <a href="?mbbank" class="banking"><img src="{{asset('assets/img/mbbank.png')}}" alt=""></a>
-                        <a href="?vietcombank" class="banking"><img src="{{asset('assets/img/vietcombank.png')}}" alt=""></a>
-                        <a href="?home-bank" class="banking"><img src="{{asset('assets/img/home-bank.png')}}" alt=""></a>
+                    <form action="" method="GET" class="select-banking">
+                        <a href="#momo" class="banking" onclick="select(this)"><img src="{{asset('assets/img/momo.png')}}" alt="momo"></a>
+                        <a href="#mbbank" class="banking" onclick="select(this)"><img src="{{asset('assets/img/mbbank.png')}}" alt="mbbank"></a>
+                        <a href="#vietcombank" class="banking" onclick="select(this)"><img src="{{asset('assets/img/vietcombank.png')}}" alt="vietcombank"></a>
+                        <a href="#home-bank" class="banking" onclick="select(this)"><img src="{{asset('assets/img/home-bank.png')}}" alt="home-bank"></a>
+                        <script>
+                            function select(element) {
+                                const radio = document.querySelectorAll('.banking');
+                                radio.forEach(input => {
+                                    input.classList.remove('action-select-banking')
+                                });  
+                                element.classList.add('action-select-banking');
+                            }
+                        </script>
                     </form>
                 </div>
             </div>
             <div class="footer">
-                <h3 class="total">Tổng tiền</h3>
-                <h3 class="total">3000000 <span>VNĐ</span></h3>
+                <h3>Tổng tiền: <span class="total-number">300.000</span> <span>VNĐ</span></h3>
                 <a href="" class="btn btn-order">Đặt hàng</a>
             </div>
         </div>
